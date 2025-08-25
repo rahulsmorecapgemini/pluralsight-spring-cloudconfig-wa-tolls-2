a2cb870e8c34451e38d54657ccc483e4758a3350
rahulsmorecapgemini <rahul.shivaji-more@capgemini.com>
2025-08-25 14:33:14 +0530

    A Message added

---
+++ src/test/java/com/pluralsight/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ConfigServerApplication.class)
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
